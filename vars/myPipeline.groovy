97066204f639ee0db6d62b979e5ef5ef9024422a
Deepakraju007 <37394734+Deepakraju007@users.noreply.github.com>
2024-04-10 22:50:03 +0530

    Update myPipeline.groovy

--- vars/myPipeline.groovy
+++ vars/myPipeline.groovy
@@ -1,61 +1,62 @@
 def call() {
-    pipeline {
-        agent any
-        stages {
-               // stage('Git Checkout') {
-                // steps {
-                //     script {
-                  //      git branch: 'node-dev', url: 'https://github.com/Deepakraju007/multi-branch.git' //project-repo
-                  // }
-                // }
-            // }
-            stage ('Build') {
-                steps {
-                    script {
-                        sh 'npm install'
-                    }
-                }
-            }
-            // stage ('SonarQube Analysis') {
-            //     steps {
-            //         script { 
-            //             def scannerHome = tool 'sonarscanner4'
-            //             withSonarQubeEnv('sonar-pro') {
-            //                 sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=node.js-app"
-            //             }
-            //         }
-            //     }
-            // }
-            stage('Docker Build Images') {
-                steps {
-                    script {
-                        sh 'docker build -t deepak5227/multi:v1 .'
-                        sh 'docker images'
-                        sh 'docker ps'
-                    }
-                }
-            }
-            // stage('Docker Push') {
-            //     steps {
-            //         script {
-            //             withCredentials([string(credentialsId: 'dockerPass', variable: 'dockerPassword')]) {
-            //                 sh "docker login -u naresh2603 -p ${dockerPassword}"
-            //                 sh 'docker push naresh2603/multi:v2'
-            //                 sh 'docker rmi naresh2603/multi:v2'
-            //             }
-            //         }
-            //     }
-            // }
-            // stage('Deploy on k8s') {
-            //     steps {
-            //         script {
-            //             withKubeCredentials(kubectlCredentials: [[ credentialsId: 'kubernetes', namespace: 'ms' ]]) {
-            //                 sh 'kubectl apply -f kube.yaml'
-            //                 sh 'kubectl get pods -o wide'
-            //             }
-            //         }
-            //     }
-            // }
-        }
+ pipeline {
+    agent any
+    tools {
+        maven 'maven3' 
     }
+    stages {
+      stage ('Build') {
+        steps {
+          script{
+            def mvnHome = tool name: 'maven3', type: 'maven'
+            sh "${mvnHome}/bin/mvn clean package"
+              sh 'mv target/onlinebookstore*.war target/mybook.war'
+              // sh "mvn clean package"
+              // sh "mv target/*.war target/mybook.war"
+          }
+        }
+      }
+      // stage ('SonarQube'){
+      //   steps{
+      //     script{
+      //       def mvnHome =  tool name: 'maven3', type: 'maven'
+      //       withSonarQubeEnv('sonar-pro') {
+      //         sh "${mvnHome}/bin/mvn sonar:sonar"
+      //       }
+      //     }
+      //   }
+      // }
+      stage('Docker Build') {
+        steps{
+          script{
+            sh 'docker build -t naresh2603/multi:v3 .'
+            //sh 'docker images'
+          }
+        }
+      }
+      stage('Docker push'){
+        steps{
+          script{
+            withCredentials([string(credentialsId: 'dockerPass', variable: 'dockerPassword')]) {
+              sh "docker login -u naresh2603 -p ${dockerPassword}"
+              sh 'docker push naresh2603/multi:v1'
+              sh 'docker rmi naresh2603/multi:v1'
+            }
+          }
+        }
+      }
+    }
+}
+
+//       stage('Deploy on k8s') {    
+//         steps {
+//           script{
+//             withKubeCredentials(kubectlCredentials: [[ credentialsId: 'kubernetes', namespace: 'ms' ]]) {
+//                 sh 'kubectl apply -f kube.yaml'
+//             }
+//           }
+//         }
+//       }
+//     }
+// }
 }
